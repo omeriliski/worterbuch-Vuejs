81e5960bdcce0f0d0b7b15a500932fc2c563bc3d
Ömer Iliski <omeriliski06@gmail.com>
2022-01-30 21:45:19 +0100

    prepaired for heroku

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import "/dist/output.css"
+import "../dist/output.css"
 import  VueFire from "vuefire";
 import store from "./store";
 const app=createApp(App);
